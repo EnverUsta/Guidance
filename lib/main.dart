4c02e9cbd8089f70f1f8a643359a5aec9ea1e7ac
Ahmet Huzeyfe Demir <35155252+koyuboy@users.noreply.github.com>
2021-12-12 13:12:55 +0300

    ts3

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() {
   
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  //tes
+  
   @override
   Widget build(BuildContext context) {
     return Sizer(
