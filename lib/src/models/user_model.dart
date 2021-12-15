38d7a0cd2358a06a6e5060923614b7aac9d8444a
Enver Usta <enveruusta@gmail.com>
2021-12-15 23:47:17 +0300

    fix: delete unnecessary properties

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -4,16 +4,12 @@ class UserModel {
   String id;
   String name;
   String email;
-  String birthDate;
-  String phoneNo;
   List<UserRole> roles;
 
   UserModel({
     required this.id,
     required this.name,
     required this.email,
-    required this.birthDate,
-    required this.phoneNo,
     required this.roles,
   });
 
@@ -21,16 +17,12 @@ class UserModel {
       : id = json['id'],
         name = json['name'],
         email = json['email'],
-        birthDate = json['birthDate'],
-        phoneNo = json['phoneNo'],
         roles = json['roles'];
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
         'email': email,
-        'birthDate': birthDate,
-        'phoneNo': phoneNo,
         'roles': roles,
       };
 }
