b74f1f32a858817e2360bbc861a00b0eecf89adb
DESKTOP-IGF01T1\Eda <eda.var@piworks.net>
2021-12-18 23:02:51 +0300

    feat: Create Guide List Item class

---
+++ lib/src/models/guide_list_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class GuideListItem {
+  String name;
+  String shortInfo;
+
+  GuideListItem({
+    required this.name,
+    required this.shortInfo,
+  });
+}
