54c81047ec161ea2b732bebc5e8979c4daf1a628
Berke <iamberkeyavas@gmail.com>
2021-12-22 21:11:45 +0300

    dev: better validation for email addresses

---
+++ lib/src/utils/helpers/validator.dart
@@ -0,0 +1,6 @@
+class Validator {
+  static bool validateEmail(String email) 
+  {
+    return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+  }
+}
